f02b384f8f096c8d530d8e27a3d51e6683d3116c
Balaguru <muraliguru99@gmail.com>
2021-10-27 14:26:56 +0530

    partials, layouts in ejs

--- files/server/server.js
+++ files/server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
 
-//register ejs view engine
-app.set('view engine', 'ejs');
 
 //dotenv config
 require('dotenv').config();
@@ -15,13 +14,29 @@ require('./DB/database')
 //body-parser deprecated
 app.use(express.json());
 
+//layouts
+app.use(expressLayouts);
+app.set('layout', './layouts/full-layout');
+
+// to access static files like css, js, images etc..
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+//register ejs view engine
+app.set('view engine', 'ejs');
+
 //routes
 app.get('/', (req, res) => {
-    res.render('login');
+    res.render('login', { title: 'Home' });
+});
+
+//dadhbiard rourte
+app.get('/dash', (req, res) => {
+    res.render('dashboard', { title: 'Dashboard', layout: './layouts/with-sidebar'});
 });
 
 //listner
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
 })
-
