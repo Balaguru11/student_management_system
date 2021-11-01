7dab9ed4b74c22e30d779e4590c3f29c2547270c
Balaguru <muraliguru99@gmail.com>
2021-11-01 18:01:08 +0530

    01-11-2021 update

--- files/server/routes/schoolRoutes.js
+++ files/server/routes/schoolRoutes.js
@@ -9,36 +9,64 @@ schoolRouter.get('/create', (req, res) => {
 
 schoolRouter.post('/create', async (req, res) => {
     try{
-        // var check = `SELECT * FROM school_add_school WHERE email='${req.body.email}'`;
-        // dbcon.query(check, (err, res) => {
-        //     if(err){
-        //         console.log("Getting error here");
-        //     }else if (res.length === 0) {
-        //             console.log("No data");
-        //     }else{
-        //         console.log("The School already exists.")
-        //     }
-        // });
-
+        var check = `SELECT EXISTS (SELECT * FROM school_add_school WHERE email='${req.body.email}') AS count`;
+        //res >> 0 / 1 
         
-            var sql = `INSERT INTO school_add_school (school_name, board, email, city, school_login, school_secrete) VALUES ("${req.body.schoolName}", "${req.body.board}", "${req.body.email}", "${req.body.schoolLocation}", "${req.body.schoolUserName}", "${req.body.schoolPassword}")`;
+        // `SELECT * FROM school_add_school WHERE email='${req.body.email}'`;
+ 
+        dbcon.query(check, (err, data) => {
+            console.log(data[0].count);
+            if(err){
+                console.log("There is an error with Database connection.");
+            }else{
+        
+                if(data[0].count == 0){
+                    const sql = `INSERT INTO school_add_school(school_name, board, email, city, school_login, school_secrete, status) VALUES ('${req.body.schoolName}', '${req.body.board}', '${req.body.email}', '${req.body.schoolLocation}', '${req.body.schoolUserName}', '${req.body.schoolPassword}', 'Inactive');`;
 
-            await dbcon.query(sql, (err, res) => {
-                if (err) {
-                    return console.log(err);
-                }
-                return res.status(200).send("Added.");
-            });
-        }catch(e) {
-            console.log(e);
-            return res.status(500).send(e);
-        }
+                        dbcon.query(sql, function (err, result) {
+                            if (err) return console.log(err);
+                            console.log(result);
+                            return res.render('school-login', {title: 'School Added'});
+                        });
+                    }else{
+                        console.log("School already Exists.");
+                        return;
+                    }
+            }
+        });
+            
+    }catch(e) {
+        console.log(e);
+        return res.status(500).send(e);
+    }
 
 });
 
-// schoolRouter.get('/login', (req, res) => {
 
-// });
+schoolRouter.get('/login', (req, res) => {
+    res.render('school-login', { title: 'School Master Login'})
+});
+
+schoolRouter.post('/login', (req, res) => {
+
+    try{
+        var loginQuery = `SELECT * FROM school_add_school WHERE school_login='${req.body.schoolUserName}' AND school_secrete='${req.body.schoolPassword}'`;
+        dbcon.query(loginQuery, function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else if (result == 1){
+                    // res.status(200).send("Logged In.");
+                    res.render('dashboard', { title: 'Dashboard'});
+                }else{
+                    res.send("No user found");
+                }
+            })
+    }catch(e){
+        console.log(e);
+        return res.status(500).send(e);
+    }
+
+});
 
 
 module.exports = schoolRouter
